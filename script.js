a4e94b46dd66a4b1db55712e3f0accc89df7ce5f
RonanPJaniola <rpjaniola28@src.pshs.edu.ph>
2025-01-22 13:02:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//variable declaration 
+    const pI = 3.14; 
+
+    var circumFerence = 2*pI*raDius;
+    var surfaceArea = 4*pI*(raDius**2);
+    var sphereVolume;
+
+    var raDius;
+//variable declaration 
