c854eb3e1375c4c4df7e1163789ddda58fc30d3a
RonanPJaniola <rpjaniola28@src.pshs.edu.ph>
2025-01-22 14:32:34 +0800

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-//variable declaration 
+// Variable declaration 
+    var raDius = 6.378e6;
     const pI = 3.14; 
 
     var circumFerence = 2*pI*raDius;
     var surfaceArea = 4*pI*(raDius**2);
-    var sphereVolume;
+    var sphereVolume = (4*pI*(raDius**3))/3;
+// Variable declaration
 
-    var raDius;
-//variable declaration 
+//Program Execution
+    document.write("The radius of the sphere is " + raDius + "." + '<br>');
+    document.write("The circumference is " + circumFerence + "." + '<br>');
+    document.write("The surface area is " + surfaceArea + "." + '<br>');
+    document.write("The volume is " + sphereVolume + "." + '<br>');
+//Program Execution
